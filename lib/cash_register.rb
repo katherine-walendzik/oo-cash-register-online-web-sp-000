17bdf854d6060081d82fb1db3f2351fb475c8082
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-06 18:15:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(item, quantity = 1, price)
+    @total += price*quantity
+    @last_transaction_amount = price * quantity
+    @total
+  end
+  
+  def items(item, quantity)
+        if quantity > 1
+      i = 0
+      while i <= quantity
+        @items << item
+        i += 1
+      end
+    else
+      @items << item
+    end
+  end
+  
+end
